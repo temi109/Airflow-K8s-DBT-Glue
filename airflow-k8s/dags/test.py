98e66de3dc829b88380deafe070a5fc173e2b968
Temidayo Ibraheem <temidayo.ibraheem@gmail.com>
2026-02-19 20:49:54 +0000

    Updated dag

--- airflow-k8s/dags/test.py
+++ airflow-k8s/dags/test.py
@@ -8,16 +8,16 @@ from airflow.sdk import DAG, task
 
 with DAG(
     dag_id="dbt_k8s_run",
-    start_date=datetime(2024,1,1),
-    schedule=None,
-    catchup=False,
+    start_date=datetime.datetime(2026, 2, 14),
+    schedule="@daily",
+    catchup=False
 ):
     @task
     def test():
         dbt_run = KubernetesPodOperator(
         task_id="run_dbt",
         name="dbt-runner",
-        namespace="default",
+        namespace="airflow",
         image="airflow-dbt:latest",
         cmds=["dbt"],
         arguments=["run"],
